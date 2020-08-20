b22b581974995ad36d8f1efc9c4f01065fb5031a
Mandisa Baleni <balenidiana@gmail.com>
2020-08-20 21:03:49 +0200

    entity deductions separate
    -separate hermit deductions to xml for entity subsumption successful:
        -loaded to test_onto successfully

--- owlready2/__MANDISA_tests/EntitySubsumption_i.py
+++ owlready2/__MANDISA_tests/EntitySubsumption_i.py
@@ -2,21 +2,22 @@ from owlready2 import *
 
 test_file_path = "/Users/mandisabaleni/PycharmProjects/MaterializationOfDeductions/testsamplemodels"
 owl_name = "EntitySubsumption_i.owl"
+owl_name_output_test = "EntitySubsumption_o2.owl"
 
 onto_path.append(test_file_path)
 onto = get_ontology(owl_name)
 onto.load()
 print(list(onto.classes()))
 print(list(onto.object_properties()))
-"""""
+print(list(onto.object_properties()))
 
-class Parent(Thing):
-  namespace = onto
-  def test(self): return "ok1"
-  def test_inherited(self): return "ok"
+test_onto =get_ontology(owl_name_output_test)
+with test_onto:
+    sync_reasoner()
+print("after reasoning")
 
-class Child(Thing):
-  namespace = onto
-  def test(self): return "ok2" 
+test_onto.save()
+#test_onto.save(file= (test_file_path + "/EntitySubsumption_o2.owl"), format="rdfxml")
+"""onto.get_children_of()
 """
 
