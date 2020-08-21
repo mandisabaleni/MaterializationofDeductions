9602635f3513e2e2a78faacaff4694498ea37506
Mandisa Baleni <balenidiana@gmail.com>
2020-08-21 23:56:15 +0200

    deduction object for subsumption successfully made

--- owlready2/preprocessMaterialize.py
+++ owlready2/preprocessMaterialize.py
@@ -1,4 +1,7 @@
+from owlready2.deduction import Deduction
 from owlready2.namespace import _open_onto_file
+from array import array
+
 
 class PreprocessMaterialize:
     def __init__(self, input_file, deductions_file, output_file):
@@ -7,6 +10,7 @@ class PreprocessMaterialize:
         self.output_file = output_file
         base_iri = '''<?xml version="1.0"?>'''
         self.deduction_object_file = _open_onto_file(base_iri, self.deductions_file, mode="rt", only_local=False)
+        self.deductions = []
     """
     reads past XML header
     """
@@ -19,13 +23,15 @@ class PreprocessMaterialize:
     """
     Finds and stores all items(classes, properties etc) to container, returns that container
     """
-    def find_items(self):#change ontology line for header part
+    def find_items(self):#change ontology line for header part, finds and stores deductions in array
         with self.deduction_object_file as f:
             start_of_header = False
             end_of_header = False
             start_of_deduction = False
             end_of_deduction = False
             end_tag = "@#$%^&RESET"
+            item_to_edit = "@#$%^&RESET"
+            whole_deduction = ""
             for i, line in enumerate(f):
                 if "Ontology" in line:
                     start_of_header = True
@@ -35,7 +41,6 @@ class PreprocessMaterialize:
                 if (start_of_header == True) and ('''</owl:Ontology>''' in line):
                     end_of_header = True
                     continue
-                item_to_edit = "@#$%^&RESET"
                 if start_of_header and end_of_header:#end of header
                     if "rdf:about=" in line:#start of deduction
                         start_of_deduction = True
@@ -46,13 +51,18 @@ class PreprocessMaterialize:
                         #print(end_tag+" <====end tag")
                         continue
                     if (start_of_deduction is True) and (end_tag in line):#end of a deduction
+                        self.deductions.append(Deduction(item_to_edit, whole_deduction))
                         end_tag = "@#$%^&RESET"
+                        item_to_edit = "@#$%^&RESET"
+                        whole_deduction = ""
                         start_of_deduction= False
                         end_of_deduction = True
-                        print("end of deduction<---" + line)
+
                     if start_of_deduction:
                         if end_of_deduction is False:#in betwwen here are the actual edits
-                           print("in between<---" + line)
+                           print("in between<---"+line )
+                           whole_deduction += line
+                           #print("whole deduction---> "+whole_deduction)
                 '''            
                 print(line)
                 print("start header---end header---start deduction---end deduction")
